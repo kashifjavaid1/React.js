9de8a7109c055881c7f082febee8f3df20265601
Muhammad Kashif <gmdcf797@gmail.com>
2024-01-28 15:48:53 +0500

    practice

--- Screen/DeatilScreen/DetailScreen.tsx
+++ Screen/DeatilScreen/DetailScreen.tsx
@@ -7,7 +7,7 @@ interface Detail{
 export default function HomeScreen(props:Detail) {
   return (
     <View>
-      {/* <Text style={{flex:1, justifyContent:'center',alignItems:'center'}}>HomeScreen 1</Text> */}
+      <Text style={{flex:1, justifyContent:'center',alignItems:'center'}}>HomeScreen 1</Text>
       <Button title='Home' onPress={()=>props.navigation.navigate('Home')}/>
     </View>
   )
