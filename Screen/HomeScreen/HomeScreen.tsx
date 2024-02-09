c13c660952a66afbb45e4c1b4a9d03bb4b226b69
Muhammad Kashif <gmdcf797@gmail.com>
2024-02-09 20:24:07 +0500

    commit

--- Screen/HomeScreen/HomeScreen.tsx
+++ Screen/HomeScreen/HomeScreen.tsx
@@ -7,7 +7,7 @@ interface butt{
 export default function HomeScreen(props:butt) {
   return (
     <View>
-      {/* <Text style={{flex:1, justifyContent:'center',alignItems:'center'}}>HomeScreen 1</Text> */}
+      <Text style={{flex:1, justifyContent:'center',alignItems:'center'}}>HomeScreen 1</Text>
       <Button title='Detail Page Move' onPress={()=>props.navigation.navigate('Detail')}/>
     </View>
   )
