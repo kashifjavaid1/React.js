ff749707bcae4d9335795af73e91bcd83ae4be11
Muhammad Kashif <gmdcf797@gmail.com>
2024-02-11 11:43:49 +0500

    practice

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,10 +1,10 @@
-import { View, Text, TextInput } from 'react-native'
+import { View, Text } from 'react-native'
 import React from 'react'
 
 export default function Navbar() {
   return (
     <View>
-     <TextInput placeholder='Search'></TextInput>
+      <Text>Navbar</Text>
     </View>
   )
 }
