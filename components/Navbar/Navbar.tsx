aa4814721af90f6375b9bfd37cdf5a29e50054a1
Muhammad Kashif <gmdcf797@gmail.com>
2024-02-25 20:15:33 +0500

    react-native

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,10 +1,16 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text} from 'react-native';
+import React from 'react';
 
 export default function Navbar() {
   return (
     <View>
       <Text>Navbar</Text>
+      <View>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ea
+        est ipsa, quae cum officiis autem, delectus itaque aliquam unde natus
+        mollitia in sunt, perferendis adipisci exercitationem eos! Inventore,
+        at.
+      </View>
     </View>
-  )
+  );
 }
