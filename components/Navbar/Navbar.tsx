bbb576b0e5762c903fab553da93731128965995f
Muhammad Kashif <gmdcf797@gmail.com>
2024-02-25 20:17:57 +0500

    React Native

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -6,10 +6,12 @@ export default function Navbar() {
     <View>
       <Text>Navbar</Text>
       <View>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Asperiores ea
-        est ipsa, quae cum officiis autem, delectus itaque aliquam unde natus
-        mollitia in sunt, perferendis adipisci exercitationem eos! Inventore,
-        at.
+        <Text>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi
+          voluptates ducimus soluta pariatur adipisci fugiat odit nam quibusdam.
+          Modi tenetur cum laborum reiciendis, non tempora aut sit assumenda
+          voluptatibus iure.
+        </Text>
       </View>
     </View>
   );
