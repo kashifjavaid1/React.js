ffefdd8d5372a3a7300e84078143d9f430ac945e
Muhammad Kashif <gmdcf797@gmail.com>
2024-04-07 20:15:28 +0500

    button-components

---
+++ Screen/(components)/button/Button.tsx
@@ -0,0 +1,14 @@
+import {View, Text, TouchableOpacity} from 'react-native';
+import React from 'react';
+
+const Button = () => {
+  return (
+    <View>
+      <TouchableOpacity>
+        <Text>Click me</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Button;
